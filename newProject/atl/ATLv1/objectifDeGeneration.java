6ce7e389f95c6836e5b70916d8c215baf138b641
E164462N <soline.lecomte@etu.univ-nantes.fr>
2020-05-07 15:10:40 +0200

    Modification fichier cible

--- newProject/atl/ATLv1/objectifDeGeneration.java
+++ newProject/atl/ATLv1/objectifDeGeneration.java
@@ -1,211 +1,198 @@
-/* 
- * Automatically generated Java code with ATL 
- */ 
  // Specific class and methods description for the node <Stopped>
-	class Stopped extends Node {
-	
+	public class Stopped extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
 // Specific class and methods description for the node <Accelerate>
-	class Accelerate extends Node {
-	
+	public class Accelerate extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
 // Specific class and methods description for the node <MoveForward>
-	class MoveForward extends Node {
-	
+	public class MoveForward extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
 // Specific class and methods description for the node <MoveBackward>
-	class MoveBackward extends Node {
-	
+	public class MoveBackward extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
 // Specific class and methods description for the node <SlowDown>
-	class SlowDown extends Node {
-	
+	public class SlowDown extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
 // Specific class and methods description for the node <ShutDown>
-	class ShutDown extends Node {
-	
+	public class ShutDown extends Node {
+
 	  @Override
-	  onEventStart(){
+	  public void onEventStart(){
 	    //TODO : Complete with the code to execute when an event is call on this node
 	  }
 
 	  @Override
-	  onEventDestination(){
+	  public void onEventDestination(){
 	    //TODO : Complete with the code to execute when an event lead to this node
 	  }
-	
+
 	  @Override
-	  onEntering(){
+	  public void onEntering(){
 	    //TODO : Complete with the code to execute when you enter in this node
 	  }
-	
+
 	  @Override
-	  onLeaving(){
+	  public void onLeaving(){
 	    //TODO : Complete with the code to execute when you leave this node
 	  }
-	
+
 	}
 
-class Application{
+public class Application{
 
   public static void main(String[] args){
     // Graph initialization
     Graph graph = new Graph();
 
     // Nodes initionazation and adding to the graph
-Stopped stopped = new Stopped();
-graph.addNode(stopped);
+    Stopped stopped = new Stopped();
+    graph.addNode(stopped);
 
-Accelerate accelerate = new Accelerate();
-graph.addNode(accelerate);
+    Accelerate accelerate = new Accelerate();
+    graph.addNode(accelerate);
 
-MoveForward moveforward = new MoveForward();
-graph.addNode(moveforward);
+    MoveForward moveforward = new MoveForward();
+    graph.addNode(moveforward);
 
-MoveBackward movebackward = new MoveBackward();
-graph.addNode(movebackward);
+    MoveBackward movebackward = new MoveBackward();
+    graph.addNode(movebackward);
 
-SlowDown slowdown = new SlowDown();
-graph.addNode(slowdown);
+    SlowDown slowdown = new SlowDown();
+    graph.addNode(slowdown);
 
-ShutDown shutdown = new ShutDown();
-graph.addNode(shutdown);
+    ShutDown shutdown = new ShutDown();
+    graph.addNode(shutdown);
 
+    // Set starting nodes of the graph
+    graph.setStartingNode(Stopped);
 
+    // Set ending nodes of the graph
+    graph.setEndingNode(ShutDown);
 
-    // Set starting and ending nodes of the graph
-    graph.setStartingNode(stopped);
-    graph.addEndingNode(shutDown);
+    //Set the transitions
+    graph.addSwitch(Stopped, Accelerate, "Accelerer");
+    graph.addSwitch(Accelerate, MoveForward, "Avancer");
+    graph.addSwitch(Accelerate, MoveBackward, "Reculer");
+    graph.addSwitch(MoveForward, SlowDown, "Ralentir");
+    graph.addSwitch(MoveBackward, SlowDown, "Ralentir");
+    graph.addSwitch(SlowDown, ShutDown, "Eteindre");
+    graph.addSwitch(Stopped, ShutDown, "Eteindre");
+    graph.addSwitch(Accelerate, ShutDown, "Eteindre");
+    graph.addSwitch(MoveBackward, ShutDown, "Eteindre");
+    graph.addSwitch(SlowDown, Stopped, "Arreter");
+    graph.addSwitch(MoveForward, ShutDown, "Eteindre");
 
-    // All posible switch from the node <stopped>
-    graph.addSwitch(stopped, accelerate, "Accelerer");
-    graph.addSwitch(stopped, shutDown, "Eteindre");
-
-    // All posible switch from the node <accelerate>
-    graph.addSwitch(accelerate, moveFoward, "Avancer");
-    graph.addSwitch(accelerate, moveBackward, "Reculer");
-    graph.addSwitch(accelerate, shutDown, "Eteindre");
-
-    // All posible switch from the node <moveFoward>
-    graph.addSwitch(moveFoward, slowDown, "Ralentir");
-    graph.addSwitch(moveFoward, shutDown, "Eteindre");
-
-    // All posible switch from the node <moveBackward>
-    graph.addSwitch(moveBackward, slowDown, "Ralentir");
-    graph.addSwitch(moveBackward, shutDown, "Eteindre");
-
-    // All posible switch from the node <SlowDown>
-    graph.addSwitch(slowDown, stopped, "Arreter");
-    graph.addSwitch(slowDown, shutDown, "Eteindre");
-
-    // AFter graph initialization, run the graph execution
+    // After graph initialization, run the graph execution
     graph.run();
-
-  }
-
+	}
 }
